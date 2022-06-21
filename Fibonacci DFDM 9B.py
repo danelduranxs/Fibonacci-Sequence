f4bfa415a81cf617a99bf4fe356a98b667d68f70
danelduranxs <107886582+danelduranxs@users.noreply.github.com>
2022-06-21 14:11:44 -0500

    Add files via upload

---
+++ Fibonacci DFDM 9B.py
@@ -0,0 +1,34 @@
+print()
+print("Números de Fibonacci entre 0 y 1000")
+print("Por Daniel Felipe Durán Martínez 9B")
+print()
+input("(Presione ENTER para iniciar)")
+print()
+
+f = 0 
+k = 1
+s = f + k 
+print(f)
+print(k)
+
+while s<987:
+  s = f + k
+  f = k 
+  k = s
+  print(s)
+   
+   
+    
+     
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
